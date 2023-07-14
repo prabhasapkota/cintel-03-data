81c8a46606efe9975d15e5e8c038afc211c70884
Prabha Sapkota <sapkotaprabha96@gmail.com>
2023-07-14 12:58:39 -0400

    Fixed errors

--- iris_ui_inputs.py
+++ iris_ui_inputs.py
@@ -1,59 +1,42 @@
-"""_summary_
-We are creating a new tab for our shiny app to display the iris dataset loaded earlier.
-The CSV file is loaded into a pandas DataFrame and the number of rows is counted.
-The server functions are defined in the get_iris_server_functions() function.
-
-"""
+from shiny import ui
 
 
-import pathlib
-from shiny import render
-import pandas as pd
-import seaborn as sns
-
-from util_logger import setup_logger
-
-logger, logname = setup_logger(__name__)
-
-
-def get_iris_server_functions(input, output, session):
-    """Define functions to create UI outputs."""
-
-    path_to_data = pathlib.Path(__file__).parent.joinpath("data").joinpath("iris.csv")
-    original_df = pd.read_csv(path_to_data)
-
-    # Use the len() function to get the number of rows in the DataFrame.
-    total_count = len(original_df)
-
-    @output
-    @render.table
-    def iris_table():
-        return original_df
-
-    @output
-    @render.text
-    def iris_record_count_string():
-        message = f"Showing {total_count} records"
-        logger.debug(f"filter message: {message}")
-        return message
-
-    @output
-    @render.plot
-    def iris_plot():
-        """
-        Use Seaborn to make a quick scatterplot.
-        Provide a pandas DataFrame and the names of the columns to plot.
-        """
-        plt = sns.scatterplot(
-            data=original_df,
-            x="sepal_length",
-            y="sepal_width",
-        )
-        return plt
-
-    # Return a list of function names for use in reactive outputs
-    functions = [
-        iris_table,
-        iris_record_count_string,
-        iris_plot,
-    ]
+def get_iris_inputs():
+    return ui.panel_sidebar(
+        ui.h2("Iris Interaction"),
+        ui.tags.hr(),
+        ui.input_slider(
+            "IRIS_PETAL_LENGTH_RANGE",
+            "Petal Length (cm)",
+            min=0,
+            max=10,
+            value=[0, 10],
+        ),
+        ui.input_slider(
+            "IRIS_PETAL_WIDTH_RANGE",
+            "Petal Width (cm)",
+            min=0,
+            max=5,
+            value=[0, 5],
+        ),
+        ui.input_slider(
+            "IRIS_SEPAL_LENGTH_RANGE",
+            "Sepal Length (cm)",
+            min=0,
+            max=8,
+            value=[0, 8],
+        ),
+        ui.input_slider(
+            "IRIS_SEPAL_WIDTH_RANGE",
+            "Sepal Width (cm)",
+            min=0,
+            max=4,
+            value=[0, 4],
+        ),
+        ui.input_checkbox("IRIS_SPECIES_Setosa", "Setosa", value=True),
+        ui.input_checkbox("IRIS_SPECIES_Versicolor", "Versicolor", value=True),
+        ui.input_checkbox("IRIS_SPECIES_Virginica", "Virginica", value=True),
+        ui.tags.hr(),
+        ui.p("🕒 Please be patient. Outputs may take a few seconds to load."),
+        ui.tags.hr(),
+    )
