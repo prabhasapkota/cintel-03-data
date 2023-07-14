fb80beeadabade16eac8cf15173d59bf3697cf92
Prabha Sapkota <sapkotaprabha96@gmail.com>
2023-07-14 12:14:00 -0400

    fixing errors

--- app.py
+++ app.py
@@ -19,9 +19,6 @@ from penguins_server import get_penguins_server_functions
 from penguins_ui_inputs import get_penguins_inputs
 from penguins_ui_outputs import get_penguins_outputs
 
-from iris_server import get_iris_server_functions
-from iris_ui_inputs import get_iris_inputs
-from iris_ui_outputs import get_iris_outputs
 
 from util_logger import setup_logger
 
@@ -54,9 +51,7 @@ app_ui = ui.page_navbar(
                     ui.tags.li(
                         "To explore the Penguins Dataset, click the 'Penguins' tab."
                     ),
-                    ui.tags.li(
-                         "To explore the Iris Dataset, click the 'Iris' tab."
-                    ),
+                    
                 ),
                 ui.tags.hr(),
                 ui.h2("Main Panel with Reactive Output"),
@@ -81,13 +76,7 @@ app_ui = ui.page_navbar(
             get_penguins_outputs(),
         ),
     ),
-    ui.nav(
-        "Iris",
-        ui.layout_sidebar(
-            get_iris_inputs(),
-            get_iris_outputs(),
-        ),
-    ),
+    
     ui.nav(ui.a("About", href="https://github.com/prabhasapkota")),
     ui.nav(ui.a("GitHub", href="https://github.com/prabhasapkota/cintel-03-data")),
     ui.nav(ui.a("App", href="https://prabha23.shinyapps.io/cintel-03-data/")),
@@ -117,7 +106,6 @@ def server(input, output, session):
 
     get_mtcars_server_functions(input, output, session)
     get_penguins_server_functions(input, output, session)
-    get_iris_server_functions(input, output, session)
-
+    
 
 app = App(app_ui, server)
--- data/app_data.py
+++ data/app_data.py
@@ -41,10 +41,6 @@ mtcars_df = pd.read_csv(data_folder.joinpath("mtcars.csv"))
 mtcars_df.to_excel(data_folder.joinpath("mtcars.xlsx"))
 
 
-iris_df = sns.load_dataset("iris")
-iris_df.to_excel(data_folder.joinpath("iris.xlsx"))
-iris_df.to_csv(data_folder.joinpath("iris.csv"))  
-
 url="https://github.com/mwaskom/seaborn-data/blob/master/iris.csv"
 # iris_df=pd.read_csv('https://github.com/mwaskom/seaborn-data/blob/master/iris.csv')
 
