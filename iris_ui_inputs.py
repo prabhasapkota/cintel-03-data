27f1082aa58af26e70d54c61538bc0fbd2ccb26e
Prabha Sapkota <sapkotaprabha96@gmail.com>
2023-07-14 16:20:54 -0400

    Updated Inputs for Iris

--- iris_ui_inputs.py
+++ iris_ui_inputs.py
@@ -16,21 +16,7 @@ def get_iris_inputs():
             max=35,
             value=[10, 35],
         ),
-         
-        ui.tags.hr(),
-        ui.tags.section(
-            ui.h3("Iris Table"),
-            ui.tags.p("Description of each field in the table:"),
-            ui.tags.ul(
-                ui.tags.li("sep_length: Length of Sepal"),
-                ui.tags.li("sep_wid: Width of Sepal"),
-                ui.tags.li("pet_len: Length of Petal"),
-                ui.tags.li("pet_wid: Width of Petal"),
-                ui.tags.li("spec: Species"),
-        ),
-            ui.output_table("irs_table"),
-        ),
-        ui.tags.hr(),
+        
         ui.p("🕒 Please be patient. Outputs may take a few seconds to load."),
         ui.tags.hr(),
     )
