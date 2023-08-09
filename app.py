754e12eccc892f9c4925b1d7ba0a489c6532637c
Prabha Sapkota <sapkotaprabha96@gmail.com>
2023-08-09 16:38:00 -0400

    Added Flights tab

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ Purpose: Use Python to create a continuous intelligence and
 interactive analytics dashboard using Shiny for Python.
 
 
-shiny has two parts:
+Each Shiny app has two parts: 
 - a user interface app_ui object (similar to the HTML in a web page) 
-- a server function that provides the logic for the app.
+- a server function that provides the logic for the app (similar to JS in a web page).
 
 """
 import shinyswatch
@@ -15,9 +15,18 @@ from mtcars_server import get_mtcars_server_functions
 from mtcars_ui_inputs import get_mtcars_inputs
 from mtcars_ui_outputs import get_mtcars_outputs
 
+from flights_server import get_flights_server_functions
+from flights_ui_inputs import get_flights_inputs
+from flights_ui_outputs import get_flights_outputs
+
 from penguins_server import get_penguins_server_functions
 from penguins_ui_inputs import get_penguins_inputs
 from penguins_ui_outputs import get_penguins_outputs
+
+from iris_server import get_iris_server_functions
+from iris_ui_inputs import get_iris_inputs
+from iris_ui_outputs import get_iris_outputs
+
 
 from util_logger import setup_logger
 
@@ -50,6 +59,9 @@ app_ui = ui.page_navbar(
                     ui.tags.li(
                         "To explore the Penguins Dataset, click the 'Penguins' tab."
                     ),
+                    ui.tags.li(
+                        "To explore the Iris Dataset, click the 'Iris' tab."
+                    ),
     ),
                 ui.tags.hr(),
                 ui.h2("Main Panel with Reactive Output"),
@@ -72,6 +84,14 @@ app_ui = ui.page_navbar(
         ui.layout_sidebar(
             get_penguins_inputs(),
             get_penguins_outputs(),
+        ),
+    ),
+    
+    ui.nav(
+        "Flights",
+        ui.layout_sidebar(
+            get_flights_inputs(),
+            get_flights_outputs(),
         ),
     ),
     
@@ -104,6 +124,9 @@ def server(input, output, session):
 
     get_mtcars_server_functions(input, output, session)
     get_penguins_server_functions(input, output, session)
+    get_iris_server_functions(input, output, session)
+    get_flights_server_functions(input, output, session)
+
     
     
 
