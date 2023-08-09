03f1e6acdc8ed6f6fcc1d3f711004f0ae4cd4fca
Prabha Sapkota <sapkotaprabha96@gmail.com>
2023-08-09 18:51:11 -0400

    Irisw Inputs arranged

--- iris_ui_inputs.py
+++ iris_ui_inputs.py
@@ -1,7 +1,3 @@
-"""
-Purpose: Provide user interaction options for the Iris dataset.
-
-"""
 from shiny import ui
 
 
@@ -10,13 +6,37 @@ def get_iris_inputs():
         ui.h2("Iris Interaction"),
         ui.tags.hr(),
         ui.input_slider(
+            "IRIS_PETAL_LENGTH_RANGE",
+            "Petal Length (cm)",
+            min=0,
+            max=10,
+            value=[0, 10],
+        ),
+        ui.input_slider(
+            "IRIS_PETAL_WIDTH_RANGE",
+            "Petal Width (cm)",
+            min=0,
+            max=5,
+            value=[0, 5],
+        ),
+        ui.input_slider(
             "IRIS_SEPAL_LENGTH_RANGE",
             "Sepal Length (cm)",
-            min=2,
+            min=0,
             max=8,
-            value=[2, 8],
+            value=[0, 8],
         ),
-        
+        ui.input_slider(
+            "IRIS_SEPAL_WIDTH_RANGE",
+            "Sepal Width (cm)",
+            min=0,
+            max=4,
+            value=[0, 4],
+        ),
+        ui.input_checkbox("IRIS_SPECIES_Setosa", "Setosa", value=True),
+        ui.input_checkbox("IRIS_SPECIES_Versicolor", "Versicolor", value=True),
+        ui.input_checkbox("IRIS_SPECIES_Virginica", "Virginica", value=True),
+        ui.tags.hr(),
         ui.p("🕒 Please be patient. Outputs may take a few seconds to load."),
         ui.tags.hr(),
     )
