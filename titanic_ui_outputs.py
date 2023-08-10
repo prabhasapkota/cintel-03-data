f964be62b6f51d040bc07de6ecb91072fbff8b07
Prabha Sapkota <sapkotaprabha96@gmail.com>
2023-08-10 19:20:32 -0400

    created outputs

---
+++ titanic_ui_outputs.py
@@ -0,0 +1,18 @@
+from shiny import ui
+from shinywidgets import output_widget
+
+
+def get_penguins_outputs():
+    return ui.panel_main(
+        ui.h2("Main Panel with Reactive Output"),
+        ui.tags.hr(),
+        ui.tags.section(
+            ui.h3("Filtered Titanic: Charts"),
+            output_widget("titanic_output_widget1"),
+            ui.tags.hr(),
+            ui.h3("Filtered Titanic Table"),
+            ui.output_text("titanic_record_count_string"),
+            ui.output_table("titanic_filtered_table"),
+            ui.tags.hr(),
+        ),
+    )
